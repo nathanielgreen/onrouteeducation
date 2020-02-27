7b3599b3f023a3f0481a89cdf6fba402a9649369
Nathaniel Green <nathanielgreen@protonmail.com>
2020-02-27 16:58:41 +0000

    Fix spacing for textarea

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,12 +1,9 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
 
 const Paragraph = styled.p`
   width: 100%;
+  white-space: pre-wrap;
 `
 
-export default ({ text }) => (
-  <Paragraph>
-    { text }
-  </Paragraph>
-)
+export default ({ text }) => <Paragraph>{text}</Paragraph>
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,9 @@ const stuff = {
     header: "Who we are.",
     content:
       "Experienced educators, Mary van der Heijden and Helen Barrett founded On Route Education as a service dedicated to helping schools successfully confront the challenges of modern, professional practice.",
-    content2:
-      "Based in the UK, our substantial experience of international education and extensive professional network means we can offer highly bespoke training, consultancy and coaching that effectively brings about positive and sustainable school improvement, leadership development and professional success. We value our relationships with schools and seek to sustain these as partners proud to be supporting their long-term learning journey",
+    content2: `Based in the UK, our substantial experience of international education and extensive professional network means we can offer highly bespoke training, consultancy and coaching that effectively brings about positive and sustainable school improvement, leadership development and professional success.
+
+We value our relationships with schools and seek to sustain these as partners proud to be supporting their long-term learning journey`,
   },
 }
 
