e140c789595ee80a7cff612293d41325c97eb5f3
Nat Green <n@ngreen.io>
2020-06-16 22:54:02 +0100

    Move home Template into own file. Add pageTemplate proptypes

---
+++ src/templates/homeTemplate.js
@@ -0,0 +1,92 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Header from "../components/Header.js";
+import Subheader from "../components/Subheader.js";
+import Paragraph from "../components/Paragraph.js";
+import Padding from "../components/Padding.js";
+import NavBox from "../components/NavBox.js";
+import Person from "../components/Person.js";
+
+import DefaultLayout from "../layouts/DefaultLayout.js";
+
+import { red } from "../utils/colors.js";
+
+const HomePageTemplate = ({ heading, services, who }) => {
+  const boxPositioning = index => {
+    switch (index) {
+      case 0:
+        return "flex-start";
+      case 2:
+        return "flex-end";
+      default:
+        return "center";
+    }
+  };
+
+  return (
+    <DefaultLayout>
+      <Header text={heading} color={red} image="images/uploads/header.jpg" />
+      <Padding>
+        <Subheader text={services.heading} />
+        <Paragraph text={services.paragraph} />
+      </Padding>
+      <Padding width="100%">
+        {services.links.map((link, index) => (
+          <NavBox
+            key={index}
+            slug={link.link}
+            text={link.text}
+            color={link.color}
+            alignSelf={boxPositioning(index)}
+            image={link.image}
+          ></NavBox>
+        ))}
+      </Padding>
+      <Padding>
+        <Subheader text={who.heading} />
+        <Paragraph text={who.paragraph1} />
+        {who.people.map((person, index) => (
+          <Person
+            key={index}
+            name={person.name}
+            title={person.title}
+            image={person.image}
+            backgroundPosition="left"
+          />
+        ))}
+        <Paragraph text={who.paragraph2} />
+      </Padding>
+    </DefaultLayout>
+  );
+};
+
+HomePageTemplate.propTypes = {
+  heading: PropTypes.string.isRequired,
+  services: PropTypes.exact({
+    heading: PropTypes.string.isRequired,
+    paragraph: PropTypes.string.isRequired,
+    links: PropTypes.arrayOf(
+      PropTypes.exact({
+        link: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        color: PropTypes.string.isRequired,
+      })
+    ),
+  }),
+  who: PropTypes.exact({
+    heading: PropTypes.string.isRequired,
+    people: PropTypes.arrayOf(
+      PropTypes.exact({
+        name: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+      })
+    ),
+    paragraph1: PropTypes.string.isRequired,
+    paragraph2: PropTypes.string.isRequired,
+  }),
+};
+
+export { HomePageTemplate };
--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -1,35 +1,42 @@
-import React from "react"
-import styled from "styled-components"
-import { graphql } from "gatsby"
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
 
-import { red, blue, orange } from "../utils/colors.js"
-import DefaultLayout from "../layouts/DefaultLayout.js"
-import Padding from "../components/Padding.js"
-import Header from "../components/Header.js"
-import Subheader from "../components/Subheader.js"
+import { red, blue, orange } from "../utils/colors.js";
+import DefaultLayout from "../layouts/DefaultLayout.js";
+import Padding from "../components/Padding.js";
+import Header from "../components/Header.js";
+import Subheader from "../components/Subheader.js";
 
 const Body = styled.div`
   min-height: 450px;
-`
+`;
 
-export default function Template({ data }) {
-  const { markdownRemark } = data
-  const { frontmatter, html } = markdownRemark
+export const PageTemplate = ({
+  frontmatter: { color, title, image },
+  html,
+}) => (
+  <DefaultLayout>
+    <Header text={title} color={color} image={image} />
+    <Padding>
+      <Subheader text={title} />
+      <Body dangerouslySetInnerHTML={{ __html: html }} />
+    </Padding>
+  </DefaultLayout>
+);
 
-  return (
-    <DefaultLayout>
-      <Header
-        text={frontmatter.title}
-        color={frontmatter.color}
-        image={frontmatter.image}
-      />
-      <Padding>
-        <Subheader text={frontmatter.title} />
-        <Body dangerouslySetInnerHTML={{ __html: html }} />
-      </Padding>
-    </DefaultLayout>
-  )
-}
+PageTemplate.propTypes = {
+  frontmatter: PropTypes.exact({
+    color: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  html: PropTypes.string.isRequired,
+};
+
+export default ({ data }) => <PageTemplate {...data.markdownRemark} />;
+
 export const pageQuery = graphql`
   query($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
@@ -42,4 +49,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
