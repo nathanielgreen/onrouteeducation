c89b306837c50dc6f8e7c273b86aa6540d7bc429
Nathaniel Green <n@ngreen.io>
2020-09-29 23:38:20 +0100

    Change person component to be a square

---
+++ src/components/Person/Person.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Person from "./Person";
+
+export default {
+  title: "Components/Person",
+  component: Person,
+};
+
+const Template = args => <Person {...args} />;
+
+export const Example = Template.bind({});
+Example.args = {
+  name: "Mary van der Heijden",
+  title: "Experienced Educator and Founder",
+  backgroundPosition: "center center",
+  image:
+    "https://images.unsplash.com/photo-1601307002864-31c17961eecd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+};
