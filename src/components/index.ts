34e621dc6d5459f44cb63a429fd2ab18190d5385
Nathaniel Green <n@ngreen.io>
2020-09-30 01:20:25 +0100

    upgrade packages and fix typescript problems

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,14 @@ import NavBox from "./NavBox/NavBox";
 import type { NavBoxProps } from "./NavBox/NavBox";
 import Person from "./Person/Person";
 import type { PersonProps } from "./Person/Person";
+import Padding from "./Padding/Padding";
+import type { PaddingProps } from "./Padding/Padding";
 
-export { Footer, Header, NavBox, Person };
-export type { FooterProps, HeaderProps, NavBoxProps, PersonProps };
+export { Footer, Header, NavBox, Person, Padding };
+export type {
+  FooterProps,
+  HeaderProps,
+  NavBoxProps,
+  PersonProps,
+  PaddingProps,
+};
